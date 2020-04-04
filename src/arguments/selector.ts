481b55794acaa503ef0b354379d2e0cad682aa17
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-04 10:11:54 +0200

    make compilation async

--- src/arguments/selector.ts
+++ src/arguments/selector.ts
@@ -67,7 +67,59 @@ export class Selector extends ArgumentObject {
     this.arguments = args;
   }
 
-  compile() {
+  async *compile() {
+    yield `@${this.target}`;
+
+    let pos = 0;
+    for (let [arg, val] of Object.entries(this.arguments)) {
+      if (pos === 0) yield "[";
+      else yield ",";
+
+      switch (arg) {
+        case "scores":
+          const scores = Object.entries<Range>(val).map(
+            ([s, r]) => `${s}=${rangeToString(r)}`
+          );
+          if (!scores.length) break;
+          yield `scores={${scores.join(",")}}`;
+          break;
+        case "level":
+          yield `level=${rangeToString(val)}`;
+          break;
+        case "team":
+        case "gamemode":
+        case "name":
+        case "type":
+        case "tag":
+        case "predicate":
+          for (let s of boolanMapToString(val, arg)) yield s;
+          break;
+        case "x_rotation":
+        case "y_rotation":
+          yield `${arg}=${rangeToString(val)}`;
+          break;
+        case "advancements":
+          type Adv = boolean | { [criteria: string]: boolean };
+          const advancements = Object.entries<Adv>(val).map(([adv, v]) => {
+            if (typeof v === "boolean") {
+              return `${adv}=${v ? "true" : "false"}`;
+            }
+            const criterias = Object.entries<boolean>(v).map(([c, v]) => {
+              return `${c}=${v ? "true" : "false"}`;
+            });
+            return `${adv}={${criterias.join(",")}}`;
+          });
+          if (!advancements.length) break;
+          yield `advancements={${advancements.join(",")}}`;
+          break;
+        default:
+          yield `${arg}=${val}`;
+      }
+    }
+    if (pos > 0) yield "]";
+  }
+
+  toString() {
     const argList: string[] = [];
     for (let [arg, val] of Object.entries(this.arguments)) {
       switch (arg) {
