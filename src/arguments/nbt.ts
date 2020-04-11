9a70428e473cdc7a7ce3a586c1db770f9345d461
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-11 13:19:29 +0200

    use nbt-ts for ntb tags

--- src/arguments/nbt.ts
+++ src/arguments/nbt.ts
@@ -1,1 +1,18 @@
-export type NBT = string;
+import { parse, stringify, Tag } from "nbt-ts";
+import { ArgumentObject } from ".";
+
+export class NBT<T extends Tag = Tag> extends ArgumentObject {
+  tag: T;
+
+  constructor(tag: T | string) {
+    super();
+    if (typeof tag === "string") {
+      tag = parse(tag) as T;
+    }
+    this.tag = tag;
+  }
+
+  toString() {
+    return stringify(this.tag);
+  }
+}
