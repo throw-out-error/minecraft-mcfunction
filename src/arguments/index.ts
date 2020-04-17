82c83f9b63f49a800cd86e077292c0ac5c647fd1
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-17 15:26:04 +0200

    added namespaced Aguments

--- src/arguments/index.ts
+++ src/arguments/index.ts
@@ -7,6 +7,12 @@ export abstract class ArgumentObject {
   public abstract toString(): string;
 }
 
+const NAMESPACE: unique symbol = Symbol("namesapce");
+export abstract class NamespacedArgument extends ArgumentObject {
+  static readonly NAMESPACE: typeof NAMESPACE = NAMESPACE;
+  [NAMESPACE]: string = "minecraft";
+}
+
 export type Argument = ArgumentObject | string | number | Range;
 
 export * from "./entity";
