095dd3f116b9571695a8a90a646b488f8a3d269f
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-05-15 13:31:46 +0200

    add compilation test

---
+++ test/compilation.js
@@ -0,0 +1,55 @@
+const assert = require("assert");
+const { McFunction, kill, Selector, give, Item, execute } = require("..");
+
+async function collect(gen) {
+  let str = "";
+  for await (let chunk of gen) {
+    str += chunk;
+  }
+  return str;
+}
+
+describe("Compilation", function () {
+  it("compiles correctly", function () {
+    function killBats() {
+      const bats = Selector.entity({
+        level: [30, Infinity],
+        type: "minecraft:bat",
+        team: {
+          red: false,
+          blue: false
+        },
+        scores: { abc: 1 },
+        distance: [undefined, 50],
+        dy: 5,
+        gamemode: { creative: true, spectator: false },
+        advancements: { d: false, e: { f: false } }
+      });
+
+      execute(kill(bats)).in("overworld");
+    }
+
+    const mcf = new McFunction(killBats);
+
+    return collect(mcf.compile()).then(str => {
+      assert.strictEqual(
+        str.trim(),
+        "execute in overworld run kill @e[type=minecraft:bat,gamemode=!spectator,gamemode=creative,team=!blue,team=!red,scores={abc=1},advancements={d=false,e={f=false}},distance=..50,dy=5,level=30..]"
+      );
+    });
+  });
+
+  it("compiles correctly", function () {
+    function giveMeCommandblock() {
+      const me = Selector.executer();
+
+      const cmdBlock = new Item("dirt");
+      give(me, cmdBlock);
+    }
+
+    const fun = new McFunction(giveMeCommandblock);
+    return collect(fun.compile()).then(str => {
+      assert.strictEqual(str.trim(), "give @s dirt");
+    });
+  });
+});
