dab6bb6879d81fc86fc134c4b124f64af392343b
Theo Paris <theoparisdesigns@gmail.com>
2020-12-15 10:16:32 -0800

    fixed src/utility.ts

--- src/utility.ts
+++ src/utility.ts
@@ -1,52 +1,53 @@
-import fs from 'fs'
-function jsonBeautify(object: object | Array<any>): string {
-  const json: string[] = JSON.stringify(object).split('')
-  let indent: number = 0
+import fs from "fs";
+
+function jsonBeautify(object: Record<string, unknown> | Array<any>): string {
+  const json: string[] = JSON.stringify(object).split("");
+  let indent: number = 0;
   for (let i: number = 0; i < json.length; i++) {
-    const char: string = json[i]
+    const char: string = json[i];
     switch (char) {
-      case '{':
-      case '[':
-        indent++
-        json.splice(i + 1, 0, '\n')
-        for (let j: number = 0; j < indent; j++) json.splice(i + j + 2, 0, '\t')
-        break
-      case '}':
-      case ']':
-        indent--
-        json.splice(i, 0, '\n')
-        for (let j: number = 0; j < indent; j++) json.splice(i + j + 1, 0, '\t')
-        i += indent + 1
-        break
-      case ',':
-        json.splice(i + 1, 0, '\n')
-        for (let j: number = 0; j < indent; j++) json.splice(i + j + 2, 0, '\t')
-        break
+      case "{":
+      case "[":
+        indent++;
+        json.splice(i + 1, 0, "\n");
+        for (let j: number = 0; j < indent; j++) json.splice(i + j + 2, 0, "\t");
+        break;
+      case "}":
+      case "]":
+        indent--;
+        json.splice(i, 0, "\n");
+        for (let j: number = 0; j < indent; j++) json.splice(i + j + 1, 0, "\t");
+        i += indent + 1;
+        break;
+      case ",":
+        json.splice(i + 1, 0, "\n");
+        for (let j: number = 0; j < indent; j++) json.splice(i + j + 2, 0, "\t");
+        break;
     }
   }
-  return json.join('')
+  return json.join("");
 }
 
 function mkdirIfNotExist(path: string) {
-  if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true })
+  if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
 }
 
 function hasIllegalChars(s: string): boolean {
-  return s != s.replace(/[^0-9a-z_\-.]/g, '')
+  return s != s.replace(/[^0-9a-z_\-.]/g, "");
 }
 
 function hasIllegalCharsSlash(s: string): boolean {
-  return s != s.replace(/[^0-9a-z_\-./]/g, '')
+  return s != s.replace(/[^0-9a-z_\-./]/g, "");
 }
 
 function itemArrayFromString(s: string): Array<any> {
   return s
-    .split('||')
-    .map(s => (s[0] == '#' ? { tag: s.slice(1) } : { item: s }))
+    .split("||")
+    .map(s => (s[0] == "#" ? { tag: s.slice(1) } : { item: s }));
 }
 
 function assumeMinecraft(s: string): string {
-  return s.includes(':') ? s : `minecraft:${s}`
+  return s.includes(":") ? s : `minecraft:${s}`;
 }
 
 export {
@@ -55,5 +56,5 @@ export {
   hasIllegalChars,
   hasIllegalCharsSlash,
   itemArrayFromString,
-  assumeMinecraft,
-}
+  assumeMinecraft
+};
